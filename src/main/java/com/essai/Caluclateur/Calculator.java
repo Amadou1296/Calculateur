fc34858e1e97fe434231cfea87998c149ade3ea3
khalil <diop25156@gmail.com>
2020-11-26 18:43:59 +0000

    jai ajouter 2 methodes min et max element

--- src/main/java/com/essai/Caluclateur/Calculator.java
+++ src/main/java/com/essai/Caluclateur/Calculator.java
@@ -17,4 +17,43 @@ public class Calculator {
 	public int divide(int a, int b) {
 		return a / b;
 	}
+
+	/**
+	 *  : retourne le plus petit élément d’une liste
+	 * @param list
+	 * @return
+	 */
+	public int minElement(int[] list)
+	{
+		int min=list[list.length-1];
+		for(int i=list.length-1;i>1;i--)
+		{
+
+			if(min>list[i-1])
+			{
+				min=list[i-1];
+			}
+		}
+		return min;
+	}
+
+
+	/**
+	 *  : retourne le plus grand élément d’une liste
+	 * @param list
+	 * @return
+	 */
+	public int maxElement(int[] list)
+	{
+		int max=list[list.length-1];
+		for(int i=list.length-1;i>1;i--)
+		{
+
+			if(max<list[i-1])
+			{
+				max=list[i-1];
+			}
+		}
+		return max;
+	}
 }
