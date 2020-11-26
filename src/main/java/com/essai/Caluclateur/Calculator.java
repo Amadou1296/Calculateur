81d7b5e62d1212a5cf6e9cc6ff34b1f004b362bb
Mamour Houma <houma.mamour@gmail.com>
2020-11-27 00:05:08 +0100

    j'ai ajouter 3 methodes min, max et multiply

--- src/main/java/com/essai/Caluclateur/Calculator.java
+++ src/main/java/com/essai/Caluclateur/Calculator.java
@@ -17,4 +17,17 @@ public class Calculator {
 	public int divide(int a, int b) {
 		return a / b;
 	}
+
+	public int multiply(int a, int b)
+	{
+		return a*b;
+	}
+	public int min(int a, int b)
+	{
+		return Math.min(a, b);
+	}
+	public int max(int a, int b)
+	{
+		return Math.max(a, b);
+	}
 }
