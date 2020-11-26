9b1d29f51ebd9982f15982515831b19773ed4c57
Amadou1296 <saradiallo44@gmail.com>
2020-11-26 17:49:03 +0000

    j'ai ajouter 3 methode somme soustraction et division

--- src/main/java/com/essai/Caluclateur/Calculator.java
+++ src/main/java/com/essai/Caluclateur/Calculator.java
@@ -1,5 +1,20 @@
 package com.essai.Caluclateur;
 
 public class Calculator {
+	
+	// addition
+	public int sum(int a,int b)
+	{
+		return a+b;
+	}
+	
+	// soustraction
+	public int minus(int a, int b) {
+		return a - b;
+	}
 
+	// division
+	public int divide(int a, int b) {
+		return a / b;
+	}
 }
