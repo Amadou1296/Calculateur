8e08b0d6cede84abad5f4e82f0feb3fdb3d97527
khalil <diop25156@gmail.com>
2020-11-28 16:03:05 +0000

    methode test

--- src/test/java/com/essai/Caluclateur/CalculatorTest.java
+++ src/test/java/com/essai/Caluclateur/CalculatorTest.java
@@ -3,6 +3,7 @@ package com.essai.Caluclateur;
 public class CalculatorTest {
     int tab[]={2,3,8,-2};
     @Test
+<<<<<<< HEAD
     public void testMultiply()
     {
 
@@ -28,4 +29,30 @@ public class CalculatorTest {
         Assert.assertEquals(-2,calculator.minElement(tab));
     }
 	
+=======
+    public void testMinelement()
+    {
+
+        Assert.assertEquals(8,calculator.maxElement(tab));
+    }
+
+    @Test
+    public void testMinElement()
+    {
+        Assert.assertEquals(-2,calculator.minElement(tab));
+    }
+
+    @Test
+    public void testMultiply()
+    {
+
+        Assert.assertEquals(12, calculator.multiply(3, 4));
+    }
+
+    @Test(expected = ArithmeticException.class)
+    public void testDivideWillThrowExceptionWhenDivideOnZero() {
+
+        calculator.divide(6, 0);
+    }
+>>>>>>> ibrahima
 }
