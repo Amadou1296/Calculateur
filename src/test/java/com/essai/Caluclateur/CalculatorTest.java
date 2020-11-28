d459cb033076f25af7fbbeb46f73f931014e2284
khalil <diop25156@gmail.com>
2020-11-28 15:48:41 +0000

    j'ai ajouté 4 methodes de test

--- src/test/java/com/essai/Caluclateur/CalculatorTest.java
+++ src/test/java/com/essai/Caluclateur/CalculatorTest.java
@@ -1,5 +1,30 @@
 package com.essai.Caluclateur;
 
 public class CalculatorTest {
-	
+    int tab[]={2,3,8,-2};
+    @Test
+    public void testMinelement()
+    {
+
+        Assert.assertEquals(8,calculator.maxElement(tab));
+    }
+
+    @Test
+    public void testMinElement()
+    {
+        Assert.assertEquals(-2,calculator.minElement(tab));
+    }
+
+    @Test
+    public void testMultiply()
+    {
+
+        Assert.assertEquals(12, calculator.multiply(3, 4));
+    }
+
+    @Test(expected = ArithmeticException.class)
+    public void testDivideWillThrowExceptionWhenDivideOnZero() {
+
+        calculator.divide(6, 0);
+    }
 }
